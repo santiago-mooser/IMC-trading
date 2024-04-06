ba551768e9598044b1990ee2a0b718035282d740
Santiago Mooser <40335919+santiago-mooser@users.noreply.github.com>
2024-04-07 00:10:49 +0800

    Update compute_volatility.py

--- utilities/compute_volatility.py
+++ utilities/compute_volatility.py
@@ -3,26 +3,26 @@ import sys
 import numpy
 import math
 
+#######
+# This file expects the formal to be the same as the activities.csv except just with one ticker
+####
+
+
 def weighted_avg_and_std(values, weights):
-    """
-    Return the weighted average and standard deviation.
-
-    They weights are in effect first normalized so that they
-    sum to 1 (and so they must not all be 0).
-
-    values, weights -- NumPy ndarrays with the same shape.
-    """
+    print(values)
     average = numpy.average(values, weights=weights)
     # Fast and numerically precise:
     variance = numpy.average((values-average)**2, weights=weights)
     return (average, math.sqrt(variance))
 
-
 filename = sys.argv[1]
 
 df = pd.read_csv(filename)
+df["spread"] = df["ask_price_1"] - df["bid_price_1"]
 
-std = weighted_avg_and_std(df['price'], df['quantity'])
+std_from_spread = df["spread"].std()
+avg_from_spread = df["spread"].mean()
 
-print(f'Weighted average: {std[0]}')
-print(f'Weighted standard deviation: {std[1]}')
+print(f'Standard deviation from mid price: {std_from_spread}')
+print(f'Mean spread: {avg_from_spread}')
+
